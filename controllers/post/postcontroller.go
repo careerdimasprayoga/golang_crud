1799e88945c4943148e9c0fcc7222b0e7c25b9da
MSahrul <sahrul@sejahteragroup.net>
2023-06-08 16:00:44 +0700

    commit 1

--- controllers/post/postcontroller.go
+++ controllers/post/postcontroller.go
@@ -4,12 +4,52 @@ import (
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/careerdimasprayoga/golang_crud/libraries"
+	"github.com/careerdimasprayoga/golang_crud/models"
+	"github.com/careerdimasprayoga/golang_crud/entities"
 )
 
-func All_post(response http.ResponseWriter, request *http.Request) {
+func all_post(response http.ResponseWriter, request *http.Request) {
 	temp, err := template.ParseFiles("views/post/all_post.html")
 	if err != nil {
 		panic(err)
 	}
 	temp.Execute(response, nil)
+}
+
+func add_post(response http.ResponseWriter, request *http.Request) {
+
+	if request.Method == http.MethodGet {
+		temp, err := template.ParseFiles("views/post/add_post.html")
+		if err != nil {
+			panic(err)
+		}
+		temp.Execute(response, nil)
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		var pasien entities.Pasien
+		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
+		pasien.NIK = request.Form.Get("nik")
+		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
+		pasien.TempatLahir = request.Form.Get("tempat_lahir")
+		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
+		pasien.Alamat = request.Form.Get("alamat")
+		pasien.NoHp = request.Form.Get("no_hp")
+
+		var data = make(map[string]interface{})
+		vErrors := validation.Struct(pasien)
+
+		if vErrors != nil {
+			data["pasien"] = pasien
+			data["validation"] = vErrors
+		} else {
+			data["pesan"] = "Data pasien berhasil disimpan"
+			pasienModel.Create(pasien)
+		}
+
+		temp, _ := template.ParseFiles("views/pasien/add.html")
+		temp.Execute(response, data)
+	}
+
 }
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http"
 	"github.com/jeypc/go-crud/controllers/pasiencontroller"
-	"controllers/postcontroller"
+	"github.com/careerdimasprayoga/golang_crud/controllers/postcontroller"
 )
 
 func main() {
@@ -11,14 +11,20 @@ func main() {
 	// http.HandleFunc("/article/", pasiencontroller.All_post)
 	// http.HandleFunc("/article", pasiencontroller.All_post)
 
-	http.HandleFunc("/all_post", postcontroller.All_post)
+	http.HandleFunc("/", postcontroller.all_post)
+	// http.HandleFunc("/add_post", postcontroller.add_post)
+	// http.HandleFunc("/all_post", postcontroller.all_post)
+	// http.HandleFunc("/drafts_post", postcontroller.drafts_post)
+	// http.HandleFunc("/trashed_post", postcontroller.trashed_post)
+	// http.HandleFunc("/edit_post", postcontroller.edit_post)
+	// http.HandleFunc("/move_post", postcontroller.move_trashed_post)
 
-	http.HandleFunc("/", pasiencontroller.Index)
-	http.HandleFunc("/pasien", pasiencontroller.Index)
-	http.HandleFunc("/pasien/index", pasiencontroller.Index)
-	http.HandleFunc("/pasien/add", pasiencontroller.Add)
-	http.HandleFunc("/pasien/edit", pasiencontroller.Edit)
-	http.HandleFunc("/pasien/delete", pasiencontroller.Delete)
+	// http.HandleFunc("/", pasiencontroller.Index)
+	// http.HandleFunc("/pasien", pasiencontroller.Index)
+	// http.HandleFunc("/pasien/index", pasiencontroller.Index)
+	// http.HandleFunc("/pasien/add", pasiencontroller.Add)
+	// http.HandleFunc("/pasien/edit", pasiencontroller.Edit)
+	// http.HandleFunc("/pasien/delete", pasiencontroller.Delete)
 
 	http.ListenAndServe(":3000", nil)
 }
