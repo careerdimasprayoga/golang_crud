5825d9695b7f98b275b890c43154c19b42382cea
MSahrul <sahrul@sejahteragroup.net>
2023-06-08 18:00:25 +0700

    dev

--- entities/post.go
+++ entities/post.go
@@ -4,6 +4,6 @@ type Post struct {
 	Id          int64
 	Title  		string `validate:"required" label:"Title"`
 	Content     string `validate:"required"`
-	Category     string `validate:"required"`
+	Category    string `validate:"required"`
 	Status 		string `validate:"required" label:"Status !"`
 }
