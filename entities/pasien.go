af73c39043ea631cf89f076f80a48a9d65dd10a1
MSahrul <sahrul@sejahteragroup.net>
2023-06-08 18:11:22 +0700

    dev

--- entities/pasien.go
+++ entities/pasien.go
@@ -3,7 +3,7 @@ package entities
 type Pasien struct {
 	Id          int64
 	Title  		string `validate:"required" label:"Title", min=20"`
-	Content     string `validate:"required" min=200"`
-	Category     string `validate:"required" min=3"`
+	Content     string `validate:"required", min=200"`
+	Category     string `validate:"required", min=3"`
 	Status 		string `validate:"required" label:"Status !"`
 }
