623bbec2487e11731facc831f09771edee55294b
MSahrul <sahrul@sejahteragroup.net>
2023-06-08 17:04:06 +0700

    dev

--- controllers/postcontroller/postcontroller.go
+++ controllers/postcontroller/postcontroller.go
@@ -4,8 +4,8 @@ import (
 	"html/template"
 	"net/http"
 	// "strconv"
-	"github.com/careerdimasprayoga/golang_crud/libraries"
-	"github.com/careerdimasprayoga/golang_crud/models"
+	// "github.com/careerdimasprayoga/golang_crud/libraries"
+	// "github.com/careerdimasprayoga/golang_crud/models"
 	"github.com/careerdimasprayoga/golang_crud/entities"
 )
 
@@ -25,30 +25,6 @@ func add_post(response http.ResponseWriter, request *http.Request) {
 			panic(err)
 		}
 		temp.Execute(response, nil)
-	} else if request.Method == http.MethodPost {
-		request.ParseForm()
-		var pasien entities.Pasien
-		pasien.NamaLengkap = request.Form.Get("nama_lengkap")
-		pasien.NIK = request.Form.Get("nik")
-		pasien.JenisKelamin = request.Form.Get("jenis_kelamin")
-		pasien.TempatLahir = request.Form.Get("tempat_lahir")
-		pasien.TanggalLahir = request.Form.Get("tanggal_lahir")
-		pasien.Alamat = request.Form.Get("alamat")
-		pasien.NoHp = request.Form.Get("no_hp")
-
-		var data = make(map[string]interface{})
-		vErrors := validation.Struct(pasien)
-
-		if vErrors != nil {
-			data["pasien"] = pasien
-			data["validation"] = vErrors
-		} else {
-			data["pesan"] = "Data pasien berhasil disimpan"
-			// pasienModel.Create(pasien)
-		}
-
-		temp, _ := template.ParseFiles("views/pasien/add.html")
-		temp.Execute(response, data)
 	}
 
 }
